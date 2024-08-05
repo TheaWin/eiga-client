bc80981b6b40cb4035b9f2cb32082610f45fd58f
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-08-05 19:57:10 +0400

    used react-router-dom and react-bootstrap

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
-
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { ProfileView } from "../profile-view/profile-view";
+import { NavigationBar } from "../navigation-bar/navigation-bar";
 
 export const MainView = () => {
 
@@ -29,7 +30,7 @@ export const MainView = () => {
           .then((data) => {
             const animeFromApi = data.map((anime) => {
               return {
-                _id: anime.id,
+                _id: anime._id,
                 Name: anime.Name,
                 Description: anime.Description,
                 imageURL: anime.imageURL,
@@ -42,82 +43,97 @@ export const MainView = () => {
           })
       }, [token]);
 
-  return (
-    <Row>
-      {!user ? (
+      return (
         <>
-          <LoginView onLoggedIn={(user,token) => {
-            setUser(user);
-            setToken(token);
-            }} 
-          />       
-          or
-          <SignupView />
-        </>
-      ) : selectedMovie ? (
-        <Col md={8}>
-          <MovieView
-            movie={selectedMovie}
-            onBackClick={() => setselectedMovie(null)}
+        <BrowserRouter>
+          <NavigationBar
+            user={user}
+            onLoggedOut={() => {
+              setUser(null);
+              setToken(null);
+              localStorage.clear();
+            }}
           />
-        </Col>
-      ) : movies.length === 0 ? (
-        <div>The list is empty!</div>
-      ) : (
-        <>
-          {movies.map((movie) => (
-            <Col key={movie._id} md={3} className="mb=5">
-              <MovieCard 
-                movie={movie} 
-                onMovieClick={(newSelectedMovie) => {
-                  setselectedMovie(newSelectedMovie)
-                }}
-              />
-            </Col>
-          ))}
+    
+          <Container>
+            <Row className="justify-content-md-center">
+              
+                <Routes>
+                  <Route
+                    path="/signup"
+                    element={
+                      user ? <Navigate to="/" /> : <SignupView />
+                    }
+                  />
+    
+                  <Route
+                    path="/login"
+                    element={
+                      user ? (
+                        <Navigate to="/" />
+                      ) : (
+                        <LoginView
+                          onLoggedIn={(user, token) => {
+                            setUser(user);
+                            setToken(token);
+                            localStorage.setItem("user", JSON.stringify(user));
+                            localStorage.setItem("token", token);
+                          }}
+                        />
+                      )
+                    }
+                  />
+    
+                  <Route
+                    path="/anime/:movieId"
+                    element={
+                      !user ? (
+                        <Navigate to="/login" replace />
+                      ) : movies.length === 0 ? (
+                        <Col> The list is empty! </Col>
+                      ) : (
+                        <Col md={8}>
+                          <MovieView movies={movies} />
+                        </Col>
+                      )
+                    }
+                  />
+    
+                  <Route
+                    path="/"
+                    element={
+                      !user ? (
+                        <Navigate to="/login" replace />
+                      ) : movies.length === 0 ? (
+                        <Col>The list is empty!</Col>
+                      ) : (
+                        <>
+                          {movies.map((movie) => (
+                            <Col className="mb-4" key={movie.id} md={3}>
+                              <MovieCard movie={movie} />
+                            </Col>
+                          ))}
+                        </>
+                      )
+                    }
+                  />
+    
+                  <Route
+                    path="/user"
+                    element={
+                      <ProfileView
+                        user={storedUser}
+                        token={storedToken}
+                        movies={movies}
+                        setUser={setUser}
+                      />
+                    }
+                  />
+                </Routes>
+              
+            </Row>
+          </Container>
+          </BrowserRouter>
         </>
       )
-      }
-    </Row>
-  )
-
-/*     if (!user) {
-      return ( 
-      <>
-      <LoginView onLoggedIn={(user,token) => {
-        setUser(user);
-        setToken(token);
-      }} />
-      or
-      <SignupView />
-      </>
-      );
-    } */
-/*     if (selectedMovie) {
-        return (
-          <MovieView
-            movie={selectedMovie}
-            onBackClick={() => setselectedMovie(null)}
-          />
-        );
-      }
-      if (movies.length === 0) {
-        return <div>The list is empty!</div>;
-      }
-      return (
-        <div>
-          {movies.map((movie) => (
-            <MovieCard
-              key={movie._id}
-              movie={movie}
-              onMovieClick={(newSelectedMovie) => {
-                setselectedMovie(newSelectedMovie);
-              }}
-            />
-          ))}
-          <button onClick={() => {setUser(null); setToken(null); localStorage.clear();}}>
-            Logout
-          </button>  
-        </div>
-    ); */
 };
