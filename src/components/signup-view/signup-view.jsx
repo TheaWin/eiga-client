09a0146efbae2f544433fa107a63338e47df2ff6
Thea Win <winwinkhaing.wk.14@gmail.com>
2024-08-05 19:56:41 +0400

    restyled with react-bootstrap

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import React, { useState } from "react";
+import { Button, Card, CardGroup, Col, Container, Form, Row } from "react-bootstrap";
 
 //SignupView function componenent
 export const SignupView = () => {
@@ -39,66 +38,79 @@ export const SignupView = () => {
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username:</Form.Label>
-        <Form.Control 
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-          minLength="3"
-          pattern="[a-zA-Z0-9]+"
-          onInvalid={(e) =>
-            e.target.setCustomValidity(
-              "Username must have at least 3 characters and be alphanumeric."
-            )}
-        />
-      </Form.Group>
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col md={8} lg={5}>
+          <CardGroup>
+            <Card className="mb-5 border border-0">
+              <Card.Body>
+                <Card.Title className="title-custom">Join us! Meow~</Card.Title>
+                  <Form onSubmit={handleSubmit}>
+                    <Form.Group controlId="formUsername" className="mb-3 mt-2">
+                      <Form.Label>Username:</Form.Label>
+                      <Form.Control 
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                        minLength="3"
+                        pattern="[a-zA-Z0-9]+"
+                        onInvalid={(e) =>
+                          e.target.setCustomValidity(
+                            "Username must have at least 3 characters and be alphanumeric."
+                          )}
+                      />
+                    </Form.Group>
 
-      <Form.Group controlId="formName">
-        <Form.Label>Name:</Form.Label>
-        <Form.Control
-          type="text" 
-          value = {name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-      </Form.Group>
+                    <Form.Group controlId="formName" className="mb-3">
+                      <Form.Label>Name:</Form.Label>
+                      <Form.Control
+                        type="text" 
+                        value = {name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                      />
+                    </Form.Group>
 
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password:</Form.Label>
-        <Form.Control
-          type="password" 
-          value = {password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </Form.Group>
+                    <Form.Group controlId="formPassword" className="mb-3">
+                      <Form.Label>Password:</Form.Label>
+                      <Form.Control
+                        type="password" 
+                        value = {password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                      />
+                    </Form.Group>
 
-      <Form.Group controlId="formEmail">
-        <Form.Label>Email:</Form.Label>
-        <Form.Control
-          type="email" 
-          value = {email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </Form.Group>
+                    <Form.Group controlId="formEmail" className="mb-3">
+                      <Form.Label>Email:</Form.Label>
+                      <Form.Control
+                        type="email" 
+                        value = {email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                      />
+                    </Form.Group>
 
-      <Form.Group controlId="formBirthday">
-        <Form.Label>Birthday:</Form.Label>
-        <Form.Control
-          type="date" 
-          value = {birthday}
-          onChange={(e) => setBirthday(e.target.value)}
-          required
-        />
-      </Form.Group>
-      
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+                    <Form.Group controlId="formBirthday" className="mb-3">
+                      <Form.Label>Birthday:</Form.Label>
+                      <Form.Control
+                        type="date" 
+                        value = {birthday}
+                        onChange={(e) => setBirthday(e.target.value)}
+                        required
+                      />
+                    </Form.Group>
+                    
+                    <Button type="submit" className="button-custom">
+                      Submit
+                    </Button>
+                  </Form>
+              </Card.Body>
+            </Card>
+          </CardGroup>
+        </Col>
+      </Row>
+    </Container>
   )
 }
